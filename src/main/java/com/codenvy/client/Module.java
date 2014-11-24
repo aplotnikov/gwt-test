724022f241fa635309aaa9e1a3ad8bb5984d7221
Andrey Plotnikov <aplotnikov@codenvy.com>
2014-11-24 22:24:32 +0200

    Fixed problem with build

--- src/main/java/com/codenvy/client/Module.java
+++ src/main/java/com/codenvy/client/Module.java
@@ -1,13 +1,28 @@
 package com.codenvy.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 
 public class Module implements EntryPoint {
 
     public void onModuleLoad() {
-        RootLayoutPanel.get().add(new Label("Hello world!"));
+        RootLayoutPanel rootLayoutPanel = RootLayoutPanel.get();
+
+        rootLayoutPanel.add(new Label("Hello world!"));
+
+        Button button = new Button("button");
+        button.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                Window.alert("Hello");
+            }
+        });
+        rootLayoutPanel.add(button);
     }
 
 }
